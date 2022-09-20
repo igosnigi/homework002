ac0fd837e24b1b6c1f3d974cc8f078a27f3cced6
igor <igor.kot.snig@yandex.ru>
2022-09-21 01:55:58 +0500

    первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("введите первое число: ");
+string input = Console.ReadLine();
+int number1 = int.Parse(input);
+
+Console.WriteLine("введите второе число: ");
+string input1 = Console.ReadLine();
+int number2 = int.Parse(input1);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max= {number1} min= {number2}");
+}
+else{
+    Console.WriteLine($"max= {number2} min= {number1}");
+}
